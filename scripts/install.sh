3aeec834d7263a575ab7806869355d4ee7eed4ad
jeffgreenca <jeff.green.ca@gmail.com>
2021-06-25 20:38:32 +0000

    change verbage and dns

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run --name garage --restart=always -p 8088:8088 -d garage
+docker stop garage && docker rm garage
+docker run --dns 8.8.8.8 --dns 8.8.4.4 --name garage --restart=always -p 8088:8088 -d garage
